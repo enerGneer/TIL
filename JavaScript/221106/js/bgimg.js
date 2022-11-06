ea2a0ca6a16f63a0b9af2cf78ee963756d9f6d46
enerGneer <energneer@gmail.com>
2022-11-06 23:06:42 +0900

    fix js-cc-random

--- JavaScript/221106/js/bgimg.js
+++ JavaScript/221106/js/bgimg.js
@@ -7,11 +7,12 @@ function bgimgView() {
   // array 숫자 가져와서 랜덤 숫자 뽑기
   const img = document.createElement("img");
   // HTML에 img 추가하기
-  document.body.append(img);
-  // img를 body 맨 아래쪽에 추가
   img.src = `img/${bgimg[i]}`;
   // img에 src 추가해주기. 경로 img폴더 및 파일이름 추가
   img.id = "bgimg";
+  // img에 id 추가해보기 test
+  document.body.prepend(img);
+  // img를 body 맨 아래쪽에 추가
 }
 
 bgimgView();
